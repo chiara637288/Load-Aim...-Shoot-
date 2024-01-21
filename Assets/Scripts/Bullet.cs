242859d78e2cbb12f191dd711a29d6564344daf2
Chiara Sanviti <chiara637288@gmail.com>
2024-01-21 18:34:59 +0100

    Bullet script

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = tranform.right * speed;
+    }
+
+} 
