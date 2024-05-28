ffff05a5db45eb8e0c4bdea9631781a9e1b05cfc
Chiara Sanviti <chiara637288@gmail.com>
2024-05-28 22:35:07 +0200

    Main Menu Buttons polish

---
+++ Assets/MainMenuMusic.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AudioManager.instance != null)
+            AudioManager.instance.FadeIn("Musica2");
+
+    }
+}
